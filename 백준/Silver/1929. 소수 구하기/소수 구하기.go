9d3b9bc0e5212f78b3cb6354daff88f3fffbccba
Chae Eun <124019895+SolidAg@users.noreply.github.com>
2023-04-17 11:19:34 +0900

    [Silver III] Title: 소수 구하기, Time: 24 ms, Memory: 2404 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.go
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.go
@@ -4,52 +4,29 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"math"
-	"sort"
 )
 
 func main() {
 	var m, n int
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Fscanln(reader, &m,&n)
 
-	var primeNumber = make(map[int]bool, n-m+1)
-	for i:=m; i<n+1; i++ {
-		primeNumber[i] = true
-	}
-	if m == 1 {
-		primeNumber[m] = false
-	}
+	r := bufio.NewReader(os.Stdin)
+	w := bufio.NewWriter(os.Stdout)
 
-	for i:=m; i<n+1; i++ {
-		var divisorCount int
-		if primeNumber[i] {
-			for j:=0; j<int(math.Sqrt(float64(i))); j++ {
-				if i%(j+1) == 0 {
-					divisorCount++
-				}
-				if divisorCount > 1 {
-					primeNumber[i] = false
-					// 에라토스테네스의 체
-					for k:=1; k*i< n+1 ; k++ {
-						primeNumber[k*i] = false
-					}
-					break
-				}
-			}
+	fmt.Fscanln(r, &m, &n)
+
+	num := make([]bool, n+1)
+
+	for i := 2; i < n+1; i++ {
+		if num[i] {
+			continue
+		}
+		if i >= m {
+			fmt.Fprintln(w, i)
+		}
+		for j := i * 2; j <= n; j += i {
+			num[j] = true
 		}
 	}
 
-	var keys []int
-	for i, v := range primeNumber {
-		if v {
-			keys = append(keys, i)
-		}
-	}
-
-	sort.Ints(keys)
-
-	for _, k := range keys {
-		fmt.Println(k)
-	}
+	w.Flush()
 }
