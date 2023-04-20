10e6a2e72cd6ba6b84df0b740d24d0e036c45aeb
Chae Eun <124019895+SolidAg@users.noreply.github.com>
2023-04-20 11:02:22 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 4 ms, Memory: 896 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.go
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.go
@@ -1,23 +1,27 @@
 package main
 
 import (
-	"fmt"
 	"bufio"
+	"fmt"
 	"os"
 )
 
 func main() {
-	var a,b,v int
+	var a, b, v int
+
 	r := bufio.NewReader(os.Stdin)
-	fmt.Fscanln(r, &a,&b,&v)
+	w := bufio.NewWriter(os.Stdout)
+
+	fmt.Fscanln(r, &a, &b, &v)
 
 	var day = 1
 
 	if (v-a)%(a-b) == 0 {
-		day += (v-a)/(a-b)
+		day += (v - a) / (a - b)
 	} else {
 		day += (v-a)/(a-b) + 1
 	}
 
-	fmt.Println(day)
+	fmt.Fprintln(w, day)
+	w.Flush()
 }
